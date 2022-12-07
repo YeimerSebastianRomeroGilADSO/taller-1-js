fbc5fbed3d7b330c1491d4d19a27110b38bb0cd6
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-12-07 11:19:35 -0500

    punto 19 js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // 18. Calcular el sueldo de un empleado dados comodatos de entrada: el nombre, hrs. De trabajo y el pago
-    // en hora. Pagohora=15300
-    let nombre = (prompt(`Ingrese su nombre`));
-    let hrs = prompt(`Ingrese las horas de trabajo realizadas`)
-    let sueldo = 15300*hrs
-    console.log(`Señor ${nombre} su sueldo por ${hrs} horas de trabajo es de ${sueldo}`);
- 
+   // 19. Un estudiante realiza cuatro exámenes. Realizar el pseudocódigo que representen el algoritmo
+    // correspondiente para obtener el promedio de las calificaciones obtenidas. las calificaciones van 1 a 5
+    // puntos.    
+    let nota1 = Number(prompt(`Ingrese el valor de la primera nota`));
+    let nota2 = Number(prompt(`Ingrese el valor de la segunda nota`));
+    let nota3 = Number(prompt(`Ingrese el valor de la tercera nota`));
+    let nota4 = Number(prompt(`Ingrese el valor de la cuarta nota`));
+    let prom = (nota1+nota2+nota3+nota4)/4;
+    if (prom<3) {
+         console.log(`No aprobo`)
+    }
+     if (prom>3) {
+         console.log (`Aprobado`)
+     }
 })
