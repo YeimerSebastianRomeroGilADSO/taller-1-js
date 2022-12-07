b07caa31bedff9f0428b63ce8a54a65995d21d1e
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-12-07 10:59:07 -0500

    punto 11 js

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-    let A = Number(prompt('Ingrese el valor del termino cuadratico'))
-    let B = Number(prompt('Ingrese el valor del termino lineal'))
-    let C = Number(prompt('Ingrese el valor del termino independiente'))
-    let formula = (B**2-4*A*C) 
-    let x1 = (-B+Math.sqrt(B**2-(4*A*C)))/((2*A))
-    let x2 = (-B-Math.sqrt(B**2-(4*A*C)))/((2*A))
-    if (formula<0){
-        console.log('No tiene soluciones reales')
-    }else if (A !=0){
-        console.log(`El valor de x1(Raiz positiva) es igual a: ${x1}`)
-        console.log(`El valor de x2(Raiz negativa) es igual a: ${x2}`)
-    }
-    else{
-        console.log(`A tiene que ser diferente de cero(0)`)
-    }
+    // 11. Escribir un programa que calcule el volumen de un elipsoide
+// V = (4/3) * PI * a * b *c
+let A = Number(prompt('Ingrese el valor del termino A'))
+let B = Number(prompt('Ingrese el valor del termino B'))
+let C = Number(prompt('Ingrese el valor del termino C'))
+let formula = ((4/3)*Math.PI*A*B*C) 
+console.log(`El resultado de (4/3)*(${Math.PI})*(${A})*(${B})*(${C}) es ${formula}`);
 })
