16c903c2d42efdb66f9eb3ab3b9cf764ffa6ee5a
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-12-07 10:48:06 -0500

    punto 7

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // 6. Escribir un programa que calcule la velocidad de un proyectil que recorre 2 Km en 5 minutos. Expresar
-    // el resultado en metros/segundo. Velocidad = espacio/tiempo
+   // 7. Escribir un programa que calcule el volumen de una esfera:
+    // Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
     let km =Number(prompt(`Ingrese los kilometros `, 2))
     let convKm = (km * 1000);
     let m = Number(prompt(`Ingrese los minutos`, 5));
     let convMin =(m * 60);
     let velocidad = (convKm / convMin);
+    console.log(`La velocidad es: ${velocidad} m/s `);
 
 })
